9c94e71c34000417e5091dee78c32592a74ef6f7
Sarah-Cashin <62601861+Sarah-Cashin@users.noreply.github.com>
2021-01-12 09:42:59 -0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Products from './Products';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+import Rating from './Rating';
+
+
+function App() {
+    const isValid = true;
+
+    return (
+        <div>
+            <h1>Learn React Hooks</h1>
+
+            <Products />
+            <Button variant="danger" disabled={!isValid}>Default</Button>
+
+
+            <Rating rating='1' />
+            <Rating rating='2' />
+            <Rating rating='3' />
+            <Rating rating='4' />
+            <Rating rating='5' />
+        </div>
+
+    );
+}
+export default App;
